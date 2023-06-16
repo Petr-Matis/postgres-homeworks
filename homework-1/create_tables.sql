a8690051ad759cc674cf5e9cffda5e3a8e340470
Petr Matis <dragon031088@gmail.com>
2023-06-16 07:30:26 +0300

    homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -4,26 +4,26 @@ CREATE DATABASE north;
 -- SQL-команды для создания таблиц
 CREATE TABLE employees
 (
-    employee_id serial PRIMARY KEY,
-    first_name varchar(20) NOT NULL,
-    last_name varchar(20) NOT NULL,
-    title varchar(50) NOT NULL,
-    birth_date date NOT NULL,
-    notes text NOT NULL
+	employees_id smallint PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
 );
 
 CREATE TABLE customers
 (
-    customer_id varchar(5) PRIMARY KEY,
-    company_name varchar(50) NOT NULL,
-    contact_name varchar(50) NOT NULL
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
 );
 
 CREATE TABLE orders
 (
-    order_id int PRIMARY KEY,
-    customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
-    employee_id int REFERENCES employees(employee_id) NOT NULL,
-    order_date date NOT NULL,
-    ship_city varchar(20) NOT NULL
+	order_id integer PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
+	employee_id integer REFERENCES employees(employees_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
 );
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,50 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+
+
+def read_file(filename):
+    """Функция чтения данных из файла"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = []
+        for line in file.readlines():
+            if filename.endswith('orders_data.csv'):
+                data.append(line[:-1].replace('"', '').split(','))
+            else:
+                data.append(line[:-1].replace('"', '').replace(', ', ' ').split(','))
+        return data
+
+
+def main():
+    """Функция коннектор-курсор"""
+    with psycopg2.connect(
+            host='localhost',
+            database='north',
+            user='postgres',
+            password=input("Введите пароль для подключение к БД: "),
+    ) as connector:
+
+        with connector.cursor() as cursor:
+            url = os.path.join("north_data", 'customers_data.csv')
+            file = read_file(url)
+            for number in range(1, len(file)):
+                print(tuple(file[number]))
+                cursor.execute('INSERT INTO customers VALUES (%s, %s, %s)', tuple(file[number]))
+
+            url = os.path.join("north_data", 'employees_data.csv')
+            file = read_file(url)
+            for number in range(1, len(file)):
+                print(tuple(file[number]))
+                cursor.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (number,) + tuple(file[number]))
+
+            url = os.path.join("north_data", 'orders_data.csv')
+            file = read_file(url)
+            for number in range(1, len(file)):
+                print(tuple(file[number]))
+                cursor.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', tuple(file[number]))
+
+    connector.close()
+
+
+if __name__ == "__main__":
+    main()
